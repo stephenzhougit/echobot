5fe54fd5321ac49466e84ecc795d5f057dbb85db
Stephen Zhou <stephenzhou@Stephens-MacBook-Pro.local>
2016-04-29 16:12:44 -0700

    change to last call and stock

--- server.js
+++ server.js
@@ -6,13 +6,27 @@ var botConnectorOptions = {
     appId: process.env.BOTFRAMEWORK_APPID, 
     appSecret: process.env.BOTFRAMEWORK_APPSECRET 
 };
+var Client = require('node-rest-client').Client;
+ 
+var client = new Client();
+ 
+// direct way 
+
 
 // Create bot
-var bot = new builder.BotConnectorBot(botConnectorOptions);
+var bot = new builder.BotConnectorBot({ appId: 'echobotnodejs', appSecret: '2a6d9ba194e14335abb3dbe8765a8a56' });
 bot.add('/', function (session) {
     
     //respond with user's message
-    session.send("You said " + session.message.text);
+    
+    client.get("http://13.82.56.114/getLastCall", function (data, response) {
+	// parsed response body as js object 
+	// console.log(data);
+    // session.send("You said " + session.message.text);
+    session.send('Call from %s!', data[0].callfrom);
+	// raw response 
+	// console.log(response);
+    });
 });
 
 // Setup Restify Server
@@ -27,6 +41,6 @@ server.get(/.*/, restify.serveStatic({
 	'default': 'index.html'
 }));
 
-server.listen(process.env.port || 3978, function () {
+server.listen(process.env.port || 8011, function () {
     console.log('%s listening to %s', server.name, server.url); 
 });
